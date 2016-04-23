2a7a6e5975c516ac3f331cb7d2e72894d3147345
sburroughs <shane.burroughs@proquest.com>
2016-04-23 13:09:01 -0500

    overview updates

--- app/components/overview/overview.component.ts
+++ app/components/overview/overview.component.ts
@@ -4,25 +4,33 @@ import {Router} from 'angular2/router';
 import {Skill} from './../skills/skill';
 import {SkillService} from './../skills/skill.service.ts';
 
+import {Experience} from './../experience/experience.ts';
 import {ExperienceComponent} from '../experience/experience.component.ts';
+import {PersonalComponent} from "../personal/personal.component";
+import {DataProviderService} from "../util/data-provider.service";
 
 @Component({
     selector: 'overview',
     templateUrl: 'app/components/overview/overview.component.html',
     styleUrls: ['assets/css/dashboard.component.css'],
-    directives: [ExperienceComponent]
+    directives: [ExperienceComponent, PersonalComponent]
 })
 export class OverviewComponent implements OnInit {
 
-    skills:Skill[] = [];
+    skillSummary:Skill[] = [];
+    latestPosition:Experience;
 
     constructor(private _router:Router,
-                private _skillService:SkillService) {
+                private _skillService:SkillService,
+                private _dataProviderService:DataProviderService) {
     }
 
     ngOnInit() {
         this._skillService.getSkills()
-            .then(skills => this.skills = skills.slice(0, 4));
+            .then(skills => this.skillSummary = skills.slice(0, 4));
+
+        this._dataProviderService.getPositions()
+            .then(experiences => this.latestPosition = experiences[0]);
     }
 
     gotoDetail(skill:Skill) {
